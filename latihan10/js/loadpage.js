1dae28b53fa78903442c0e304b6121fa92b69dd6
fitripuspitasari <fitripuspitasari_19@kharisma.ac.id>
2021-02-09 20:38:29 +0700

    addd

--- latihan10/js/loadpage.js
+++ latihan10/js/loadpage.js
@@ -11,24 +11,23 @@ document.addEventListener("DOMContentLoaded", function() {
         var content = document.querySelector("#body-content");
         if (this.status == 200) {
           content.innerHTML = xhttp.responseText;
-
-          document.querySelectorAll(".collection-item, .topnav a, .sidenav a")
+          document.querySelectorAll(".collection-item, .topnav a, .sidenav a, .nav")
           .forEach(function(element) {
             element.addEventListener("click", function(event) {
               page = event.target.getAttribute("href").substr(1);
               loadPage(page);
             });
-            });
-
-        } else if (this.status == 404) {
+          });
+        }
+        else if (this.status == 404) {
           content.innerHTML = "<p>Halaman <strong>" + page + "</strong> tidak ditemukan.</p>";
-        } else {
+        }
+        else {
           content.innerHTML = "<p>Ups.. halaman <strong>" + page + "</strong> tidak dapat diakses.</p>";
         }
       }
     };
-
     xhttp.open("GET", "pages/" + page + ".html", true);
     xhttp.send();
-}
-  });
+  }
+});
